b2415cb5389182ee57bf2fb9f6200028e5329ba7
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-30 16:00:10 -0700

    Update the best thing 5.py

--- the best thing 5.py
+++ the best thing 5.py
@@ -42,20 +42,20 @@ if st.button("تأكيد التخمين "):
   else:
    st.session_state.num =0
    st.session_state.feed="false" 
-if st.session_state.feed==correct:
- st.success("انك اسطوره يا عبقري الرياضه ")
- st.session_state.num1=random.randint(1,st.session_state.ran)
- st.session_state.num2=random.randint(1,st.session_state.ran)
- st.session_state.sign=random.choice(['+','-','*','/'])
- time.sleep(1)
- st.rerun()
-if st.session_state.feed==false:
- st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
- st.session_state.num1=random.randint(1,st.session_state.ran)
- st.session_state.num2=random.randint(1,st.session_state.ran)
- st.session_state.sign=random.choice(['+','-','*','/'])
- time.sleep(1)
- st.rerun()   
+  if st.session_state.feed=="correct":
+   st.success("انك اسطوره يا عبقري الرياضه ")
+   st.session_state.num1=random.randint(1,st.session_state.ran)
+   st.session_state.num2=random.randint(1,st.session_state.ran)
+   st.session_state.sign=random.choice(['+','-','*','/'])
+   time.sleep(1)
+   st.rerun()
+ if st.session_state.feed=="false":
+  st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
+  st.session_state.num1=random.randint(1,st.session_state.ran)
+  st.session_state.num2=random.randint(1,st.session_state.ran)
+  st.session_state.sign=random.choice(['+','-','*','/'])
+  time.sleep(1)
+  st.rerun()   
 
 if st.session_state.num > 0 and st.session_state.num % 10 == 0:
   st.success("انت بطل! تحدي صديقك انه بالطبع لن يستطيع ان يصل لمستواك  ")
