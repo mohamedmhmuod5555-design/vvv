938d4d1fa7565ca23f4fa913757f15bdcfeca579
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-30 16:11:04 -0700

    Update the best thing 5.py

--- the best thing 5.py
+++ the best thing 5.py
@@ -44,21 +44,14 @@ if st.button("تأكيد التخمين "):
    st.session_state.feed="false" 
 if st.session_state.feed=="correct":
   st.success("انك اسطوره يا عبقري الرياضه ")
-  del st.session_state.num1
-  del st.session_state.num2
-  del st.session_state.sign
-  time.sleep(1)
-  st.rerun()
   
 if st.session_state.feed=="false":
   st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
-  del st.session_state.num1
-  del st.session_state.num2
-  del st.session_state.sign
-  time.sleep(1)
-  st.rerun()
-     
-
+del st.session_state.num1
+del st.session_state.num2
+del st.session_state.sign
+time.sleep(1)
+st.rerun()
 if st.session_state.num > 0 and st.session_state.num % 10 == 0:
   st.success("انت بطل! تحدي صديقك انه بالطبع لن يستطيع ان يصل لمستواك  ")
   if st.button("الليفل التالي "):
