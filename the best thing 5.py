54c229dcb528d16c2a0ef1bf1e1e3e3f71e06da6
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-30 16:02:09 -0700

    Update the best thing 5.py

--- the best thing 5.py
+++ the best thing 5.py
@@ -36,20 +36,20 @@ st.write(num1,sign,num2)
 number=st.number_input("ادخل النتيجه ")
 if st.button("تأكيد التخمين "):
   st.session_state.count += 1
-  if number == sc:
+ if number == sc:
    st.session_state.num += 1
    st.session_state.feed="correct"
   else:
    st.session_state.num =0
    st.session_state.feed="false" 
-  if st.session_state.feed=="correct":
-   st.success("انك اسطوره يا عبقري الرياضه ")
-   st.session_state.num1=random.randint(1,st.session_state.ran)
-   st.session_state.num2=random.randint(1,st.session_state.ran)
-   st.session_state.sign=random.choice(['+','-','*','/'])
-   time.sleep(1)
-   st.rerun()
- if st.session_state.feed=="false":
+if st.session_state.feed=="correct":
+  st.success("انك اسطوره يا عبقري الرياضه ")
+  st.session_state.num1=random.randint(1,st.session_state.ran)
+  st.session_state.num2=random.randint(1,st.session_state.ran)
+  st.session_state.sign=random.choice(['+','-','*','/'])
+  time.sleep(1)
+  st.rerun()
+if st.session_state.feed=="false":
   st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
   st.session_state.num1=random.randint(1,st.session_state.ran)
   st.session_state.num2=random.randint(1,st.session_state.ran)
