25baf0fe6be6eb541d45df27a0abd8af84d7ea61
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-30 16:03:07 -0700

    Update the best thing 5.py

--- the best thing 5.py
+++ the best thing 5.py
@@ -48,14 +48,14 @@ if st.session_state.feed=="correct":
   st.session_state.num2=random.randint(1,st.session_state.ran)
   st.session_state.sign=random.choice(['+','-','*','/'])
   time.sleep(1)
-  st.rerun()
+  
 if st.session_state.feed=="false":
   st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
   st.session_state.num1=random.randint(1,st.session_state.ran)
   st.session_state.num2=random.randint(1,st.session_state.ran)
   st.session_state.sign=random.choice(['+','-','*','/'])
   time.sleep(1)
-  st.rerun()   
+     
 
 if st.session_state.num > 0 and st.session_state.num % 10 == 0:
   st.success("انت بطل! تحدي صديقك انه بالطبع لن يستطيع ان يصل لمستواك  ")
