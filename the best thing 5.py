c0ad99708012a70450c0fb8682c33ed052d21bb5
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-30 16:02:28 -0700

    Update the best thing 5.py

--- the best thing 5.py
+++ the best thing 5.py
@@ -36,7 +36,7 @@ st.write(num1,sign,num2)
 number=st.number_input("ادخل النتيجه ")
 if st.button("تأكيد التخمين "):
   st.session_state.count += 1
- if number == sc:
+  if number == sc:
    st.session_state.num += 1
    st.session_state.feed="correct"
   else:
